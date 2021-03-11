48a602e54af40e38963ebd73c2acc554cddabb18
Antonio Valenti <valentiantonio89@gmail.com>
2021-03-11 17:48:06 +0100

    fileToBase64 types

--- src/utils/fileToBase64.ts
+++ src/utils/fileToBase64.ts
@@ -1,5 +1,5 @@
 export const fileToBase64 = (file: File) =>
-  new Promise((resolve, reject) => {
+  new Promise<string | ArrayBuffer | null>((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => resolve(reader.result);
