513c7094ca8cae17dc7357bcc6a80bdca9db8878
Antonio Valenti <valentiantonio89@gmail.com>
2021-04-01 15:28:03 +0200

    Hooks | useForwardedRef

---
+++ src/hooks/useForwardedRef.ts
@@ -0,0 +1,15 @@
+import { MutableRefObject, useEffect, useRef } from 'react';
+
+export const useForwardedRef = <T extends HTMLElement>(ref: React.Ref<T>) => {
+	const innerRef = useRef<T>(null);
+	useEffect(() => {
+		if (!ref) return;
+		if (typeof ref === 'function') {
+			ref(innerRef.current);
+		} else {
+			(ref as MutableRefObject<T | null>).current = innerRef.current;
+		}
+	});
+
+	return innerRef;
+};
