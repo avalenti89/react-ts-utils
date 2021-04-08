f4a4d0ecbb8cec4df61823283c0c0f00fe2b2767
Antonio Valenti <valentiantonio89@gmail.com>
2021-04-08 16:37:18 +0200

    Utils | targetTimeout onTick with clear function

--- src/utils/targetTimeout.ts
+++ src/utils/targetTimeout.ts
@@ -1,10 +1,9 @@
 export const targetTimeout = (
 	_targetTime: Date | string | number,
 	callback: () => void,
-	options?: { precision?: number; onTick?: () => void }
+	options?: { precision?: number; onTick?: (stop: () => void) => void }
 ) => {
 	const { precision, onTick } = options ?? {};
-	onTick?.();
 	let targetTime: Date;
 	if (typeof _targetTime === 'string' || typeof _targetTime === 'number') {
 		targetTime = new Date(_targetTime);
@@ -15,8 +14,9 @@ export const targetTimeout = (
 	if (Date.now() >= targetTime.valueOf()) {
 		callback();
 	} else {
-		window.setTimeout(() => {
+		const timeoutc = window.setTimeout(() => {
 			targetTimeout(targetTime, callback, options);
 		}, precision ?? 1000);
+		onTick?.(() => window.clearTimeout(timeoutc));
 	}
 };
