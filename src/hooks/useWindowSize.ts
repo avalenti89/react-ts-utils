1ed35060779c6c34253d3641c49021810eeafbda
Antonio Valenti <valentiantonio89@gmail.com>
2021-04-15 12:51:00 +0200

    Hooks | removes named tuples

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,28 +1,31 @@
 import { useCallback, useLayoutEffect, useState } from 'react';
 import { Size } from '../types/Size';
-
-export const useWindowSize = ():[size:Size| null, start:()=>void, stop:()=>void] => {
+/**
+ *
+ * @returns [Size, start(), stop()]
+ */
+export const useWindowSize = (): [Size | null, () => void, () => void] => {
 	const [windowSize, setWindowSize] = useState<Size | null>(null);
 
-	const handleResize=useCallback(()=>{
+	const handleResize = useCallback(() => {
 		setWindowSize({
 			width: window.innerWidth,
 			height: window.innerHeight
 		});
-	},[]);
+	}, []);
 
-	const startListener = useCallback(()=>{
+	const startListener = useCallback(() => {
 		window.addEventListener('resize', handleResize);
-	},[handleResize])
-	
-	const stopListener = useCallback(()=>{
-		window.removeEventListener('resize', handleResize)
-	},[handleResize]);
+	}, [handleResize]);
+
+	const stopListener = useCallback(() => {
+		window.removeEventListener('resize', handleResize);
+	}, [handleResize]);
 
 	useLayoutEffect(() => {
 		handleResize();
 		return stopListener;
-	}, [handleResize,startListener,stopListener]);
+	}, [handleResize, startListener, stopListener]);
 
-	return [windowSize,startListener,stopListener];
+	return [windowSize, startListener, stopListener];
 };
