4f4a771020913efca71f2efa216a716ec2f81eb5
Antonio Valenti <valentiantonio89@gmail.com>
2021-12-16 14:42:53 +0100

    Components | Modal provider

---
+++ src/components/Modal/Modal.provider.tsx
@@ -0,0 +1,49 @@
+import { IPortalProps, ToggablePortal } from 'components';
+import React, { createContext, useCallback, useEffect, useState } from 'react';
+
+type IModalContextState = {
+	isOpen: boolean;
+};
+type IModalContextCallback = {
+	open: () => void;
+	close: () => void;
+	toggle: (isOpen: boolean) => void;
+};
+
+type IModalContext = IModalContextState & IModalContextCallback;
+
+export const modalContext = createContext({} as IModalContext);
+
+export type IModalProviderProps = Partial<IModalContextState> & {
+	onToggle?: (isOpen: boolean) => void;
+	target: IPortalProps['target'];
+};
+
+export const ModalProvider = ({
+	isOpen: _isOpen = false,
+	onToggle,
+	target,
+	children,
+}: React.PropsWithChildren<IModalProviderProps>) => {
+	const [isOpen, setIsOpen] = useState(_isOpen);
+
+	useEffect(() => {
+		setIsOpen(_isOpen);
+	}, [_isOpen]);
+
+	useEffect(() => {
+		onToggle?.(isOpen);
+	}, [isOpen]);
+
+	const open = () => setIsOpen(true);
+	const close = () => setIsOpen(false);
+	const toggle = (value: boolean) => setIsOpen((prev) => value ?? !prev);
+
+	return (
+		<modalContext.Provider value={{ isOpen: _isOpen, open, close, toggle }}>
+			<ToggablePortal target={target} isActive={_isOpen}>
+				{children}
+			</ToggablePortal>
+		</modalContext.Provider>
+	);
+};
